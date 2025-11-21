6a7413724b5ad16fa7d87767164e4d4cc21803c0
Piotrr0 <piotrr.nawalany@gmail.com>
2025-11-21 09:21:58 +0000

    clean up codegen

--- src/codegen.h
+++ src/codegen.h
@@ -8,6 +8,7 @@
 #include <llvm-c/BitWriter.h>
 #include <llvm-c/Types.h>
 
+#include "ast_layout.h"
 #include "lookup_table.h"
 #include "parser.h"
 
@@ -24,16 +25,15 @@ extern LLVMValueRef current_function;
 extern SymbolTable* st;
 
 LLVMTypeRef build_param_type(TypeInfo* type_info);
-void collect_function_param_types(FunctionNode* func_node, LLVMTypeRef* param_types);
 
 void init_codegen(CodegenContext* ctx, const char* module_name);
 void cleanup_codegen(CodegenContext* ctx);
 LLVMTypeRef token_type_to_llvm_type(CodegenContext* ctx, TokenType type);
 
-void codegen_struct_declaration(ASTNode* node);
+void codegen_struct_declaration(StructDeclNode struct_decl_node);
 LLVMValueRef codegen_member_access(ASTNode* node);
 
-void codegen_if(ASTNode* node);
+void codegen_if(IfNode if_node);
 void codegen_then_block(ASTNode* node_block, LLVMBasicBlockRef mergeBB);
 void codegen_else_block(ASTNode* node_else, LLVMBasicBlockRef mergeBB);
 
@@ -45,20 +45,19 @@ LLVMValueRef codegen_less_equal(LLVMValueRef left, LLVMValueRef right, LLVMTypeK
 LLVMValueRef codegen_greater_equal(LLVMValueRef left, LLVMValueRef right, LLVMTypeKind type);
 
 LLVMValueRef codegen_constant(ASTNode* node);
-LLVMValueRef codegen_string_literal(ASTNode* node);
-LLVMValueRef codegen_char_literal(ASTNode* node);
+
+LLVMValueRef codegen_string_literal(StringLiteralNode string_node);
+LLVMValueRef codegen_char_literal(CharLiteralNode char_node);
+LLVMValueRef codegen_float_literal(FloatLiteralNode float_node);
+LLVMValueRef codegen_int_literal(IntLiteralNode int_node);
+LLVMValueRef codegen_double_literal(DoubleLiteralNode double_node);
+
 LLVMValueRef codegen_variable_load(const char* name);
-LLVMValueRef codegen_cast(ASTNode* node);
-LLVMValueRef codegen_function_call(ASTNode* node);
+LLVMValueRef codegen_cast(CastNode cast_node);
+LLVMValueRef codegen_function_call(FuncCallNode function_call_node);
 
-LLVMValueRef codegen_pre_inc_dec(ASTNode* node);
-LLVMValueRef codegen_pre_dec(ASTNode* node);
-LLVMValueRef codegen_post_inc(ASTNode* node);
-LLVMValueRef codegen_post_dec(ASTNode* node);
-
-
-void codegen_while_loop(ASTNode* node);
-void codegen_for_loop(ASTNode* node);
+void codegen_while_loop(WhileNode while_node);
+void codegen_for_loop(ForNode for_node);
 
 int does_type_kind_match(LLVMValueRef left, LLVMValueRef right, LLVMTypeKind* out_kind);
 LLVMValueRef codegen_arithmetic_op(ASTNode* node);
@@ -72,23 +71,30 @@ int are_types_compatible(LLVMTypeRef form_type, LLVMTypeRef to_type);
 LLVMValueRef generate_cast_instruction(LLVMValueRef value, LLVMTypeRef from_type, LLVMTypeRef to_type, const char* name);
 
 void codegen_module_id(const char* name);
-void codegen_program(ASTNode* node);
+void codegen_program(ProgramNode program_node);
 
-void setup_function_params(LLVMValueRef function, FunctionNode* func_node);
-void generate_function_body(ASTNode* body_node);
-void codegen_function(ASTNode* node);
+void codegen_function(FunctionNode func_node);
+void setup_function_params(LLVMValueRef function, FunctionNode func_node);
+void collect_function_param_types(FunctionNode func_node, LLVMTypeRef* param_types);
+void generate_function_body(BlockNode body_node);
 
-void codegen_block(ASTNode* node);
-void codegen_statement(ASTNode* node);
-void codegen_return(ASTNode* node);
-void codegen_variable_declaration(ASTNode* node);
-void codegen_global_variable_declaration(ASTNode* node);
-void codegen_assign(ASTNode* node);
-
-LLVMValueRef codegen_negation(ASTNode* node);
 LLVMValueRef codegen_expression(ASTNode* node);
-LLVMValueRef codegen_dereference(ASTNode* node);
-LLVMValueRef codegen_address_of(ASTNode* node);
+void codegen_block(BlockNode block_node);
+void codegen_statement(ASTNode* node);
+void codegen_return(ReturnNode return_node);
+void codegen_variable_declaration(VarDeclNode var_decl_node);
+void codegen_global_variable_declaration(VarDeclNode var_decl_node);
+void codegen_assign(AssignNode assign_node);
+
+LLVMValueRef codegen_unary_op(UnaryOpNode node);
+LLVMValueRef codegen_negation(UnaryOpNode node);
+LLVMValueRef codegen_dereference(UnaryOpNode node);
+LLVMValueRef codegen_address_of(UnaryOpNode node);
+LLVMValueRef codegen_pre_inc(UnaryOpNode node);
+LLVMValueRef codegen_pre_dec(UnaryOpNode node);
+LLVMValueRef codegen_post_inc(UnaryOpNode node);
+LLVMValueRef codegen_post_dec(UnaryOpNode node);
+
 
 void generate_llvm_ir(ASTNode* ast, const char* module_name, const char* output_filename);
 
