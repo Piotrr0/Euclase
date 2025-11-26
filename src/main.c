2787e05b8f8f5f2a2401181172145fa8973dd757
Piotrr0 <piotrr.nawalany@gmail.com>
2025-11-26 10:56:08 +0000

    codegen struct member access

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "codegen_visitor.h"
 #include "lexer.h"
 #include "parser.h"
-#include "tests.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,12 +8,12 @@
 int has_ecl_extension(const char* filename) {
     if (filename == NULL)
         return 0;
-    
+
     size_t len = strlen(filename);
     size_t len_ext = strlen(".ecl");
     if (len < len_ext + 1) 
         return 0;
-    
+
     return strcmp(filename + len - len_ext, ".ecl") == 0;
 }
 
@@ -35,27 +34,27 @@ char* get_module_name(const char* filename)
 {
     if (filename == NULL)
         return NULL;
-    
+
     const char* basename = filename;
     const char* last_backslash = strrchr(filename, '\\');
     
     if (last_backslash != NULL)
         basename = last_backslash + 1;
-    
+
     size_t name_len;
     const char* last_dot = strrchr(basename, '.');    
     if (last_dot != NULL)
         name_len = last_dot - basename;
     else
         name_len = strlen(basename);
-    
+
     char* module_name = malloc(name_len + 1);
     if (module_name == NULL)
         return NULL;
-    
+
     strncpy(module_name, basename, name_len);
     module_name[name_len] = '\0';
-    
+
     return module_name;
 }
 
@@ -63,12 +62,12 @@ char* get_output_filename(const char* module_name)
 {
     if (module_name == NULL)
         return NULL;
-    
+
     size_t len = strlen(module_name);
     char* output = malloc(len + strlen(".ll") + 1);
     if (output == NULL)
         return NULL;
-    
+
     sprintf(output, "%s.ll", module_name);
     return output;
 }
@@ -82,7 +81,7 @@ char* get_source_from_file(int argc, char** argv) {
         print_usage();
         return NULL;
     }
-    
+
     FILE* fptr = fopen(filename, "r");
     if (fptr == NULL) {
         return NULL;
@@ -92,29 +91,29 @@ char* get_source_from_file(int argc, char** argv) {
         fclose(fptr);
         return NULL;
     }
-    
+
     long fsize = ftell(fptr);
     if (fsize <= 0) {
         fclose(fptr);
         return NULL;
     }
-    
+
     fseek(fptr, 0, SEEK_SET);
-    
+
     char* code = malloc(fsize + 1);
     if (code == NULL) {
         fclose(fptr);
         return NULL;
     }
-    
+
     size_t bytes_read = fread(code, 1, fsize, fptr);
     fclose(fptr);
-    
+
     if (bytes_read != fsize) {
         free(code);
         return NULL;
     }
-    
+
     code[fsize] = '\0';
     return code;
 }
@@ -124,20 +123,20 @@ void compile_source_file(int argc, char** argv)
     char* code = get_source_from_file(argc, argv);
     if (code == NULL)
         return;
-    
+
     char* module_name = get_module_name(argv[1]);
     if (module_name == NULL) {
         free(code);
         return;
     }
-    
+
     char* output_filename = get_output_filename(module_name);
     if (output_filename == NULL) {
         free(module_name);
         free(code);
         return;
     }
-    
+
     Lexer lexer;
     Tokens* tokens = tokenize(&lexer, code, 1);
     cleanup_lexer(&lexer);
@@ -155,9 +154,5 @@ void compile_source_file(int argc, char** argv)
 
 int main(int argc, char** argv) {
     compile_source_file(argc, argv);
-
-#ifdef __unix__
-    run_tests();
-#endif
     return 0;
 }
